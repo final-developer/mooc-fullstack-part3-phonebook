3ff4857d10631983ab3893bfc7bf3321b74b5ec6
LG <lukasgruler@web.de>
2024-04-23 16:57:05 +0200

    extend functionality (ex 3.19.-3.21.)

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 
@@ -14,102 +15,95 @@ app.use(express.static('dist'))
 const cors = require('cors')
 app.use(cors())
 
+// MongoDB and Mongoose handling
+const Person = require('./modules/person')
 
 let entries = [
-   {
-      "id": 1,
-      "name": "Arto Hellas",
-      "number": "040-123456"
-   },
-   {
-      "id": 2,
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523"
-   },
-   {
-      "id": 3,
-      "name": "Dan Abramov",
-      "number": "12-43-234345"
-   },
-   {
-      "id": 4,
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122"
-   },
-   {
-      "id": 5,
-      "name": "Max Mustermann",
-      "number": "46987156854798"
-   }
+
 ]
 
 // get all entries
 app.get('/api/persons', (request, response) => {
-   response.json(entries)
+   Person.find({}).then(persons => {
+      response.json(persons)
+   })
 })
 
 // get single entry
-app.get('/api/persons/:id', (request, response) => {
-   const id = Number(request.params.id)
-   const person = entries.find(entry => entry.id === id)
-
-   if (person) {
-      response.json(person)
-   } else {
-      response.status(404).send()
-   }
+app.get('/api/persons/:id', (request, response, next) => {
+   Person.findById(request.params.id)
+      .then(person => {
+         response.json(person)
+      })
+      .catch(error => next(error))
 })
 
 // delete single entry
-app.delete('/api/persons/:id', (request, response) => {
-   const id = Number(request.params.id)
-   entries = entries.filter(entry => entry.id !== id)
+app.delete('/api/persons/:id', (request, response, next) => {
+   // const id = Number(request.params.id)
+   // entries = entries.filter(entry => entry.id !== id)
 
-   response.status(204).end()
+   // response.status(204).end()
+   Person.findByIdAndDelete(request.params.id)
+      .then(result => response.status(204).end())
+      .catch(error => next(error))
 })
 
-// generate random id
-function getRandomId(max) {
-   return Math.floor(Math.random() * max);
-}
-
 // add single entry
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
    const body = request.body
 
-   if (!body.name || !body.number) {
-      return response.status(400).json({
-         error: 'name or number missing'
-      })
-   }
-
-   if (entries.filter(entry => entry.name === body.name).length > 0) {
-      return response.status(400).json({
-         error: 'name already exists'
-      })
-   }
-
-   const entry = {
-      id: getRandomId(100000),
+   const person = new Person({
       name: body.name,
-      number: body.number
-   }
+      number: body.number,
+   })
+   person.save()
+      .then(savedPerson => response.json(savedPerson))
+      .catch(error => next(error))
+})
 
-   entries = entries.concat(entry)
-   response.json(entry)
+// update single entry
+app.put('/api/persons/:id', (request, response, next) => {
+   const { name, number } = request.body
+
+   Person.findByIdAndUpdate(
+      request.params.id,
+      { name, number },
+      { new: true, runValidators: true, context: 'query' }
+   )
+      .then(updatedPerson => response.json(updatedPerson))
+      .catch(error => next(error))
 })
 
 // get info page
 app.get('/info', (request, response) => {
-   const entryCount = entries.length
-   const timestamp = new Date().toString()
-   response.send(
-      `
-      <p>Phonebook has info for ${entryCount} people</p>
-      <p>${timestamp}</p>
-      `
-   )
+   Person.find({}).then(persons => {
+      const entryCount = persons.length
+      const timestamp = new Date().toString()
+      response.send(
+         `
+         <p>Phonebook has info for ${entryCount} people</p>
+         <p>${timestamp}</p>
+         `
+      )
+   })
+
 })
+
+// error handler middleware (because of 4 parameters)
+const errorHandler = (error, request, response, next) => {
+   console.error(error.message)
+
+   if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+   } else if (error.name === 'ValidationError') {
+      return response.status(400).json({ error: error.message })
+   }
+
+   // pass error to default express error handler
+   next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
---
+++ mongo.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose')
+
+// define schema
+const personSchema = new mongoose.Schema({
+   id: Number,
+   name: String,
+   number: String,
+})
+const Person = mongoose.model('Person', personSchema)
+
+// get all persons from db
+const getPersons = () => {
+   Person.find({}).then(result => {
+      console.log("phonebook:")
+      result.forEach(person => {
+         console.log(`${person["name"]} ${person["number"]}`)
+      })
+      mongoose.connection.close()
+   })
+}
+
+// add person to db
+const addPerson = () => {
+   // generate random id
+   function getRandomId(max) {
+      return Math.floor(Math.random() * max);
+   }
+
+   // adding new person to phonebook
+   const person = new Person({
+      id: getRandomId(100000),
+      name: name,
+      number: number,
+   })
+   person.save().then(result => {
+      console.log(`added ${result["name"]} number ${result["number"]} to phonebook`)
+      mongoose.connection.close()
+   })
+}
+
+// handle console input / arguments
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+if (process.argv.length < 3) {
+   console.log('required arguments: <password> (optional: <name> <phonenumber>)')
+   process.exit(1)
+}
+if (process.argv.length === 3) {
+   getPersons()
+} else if (process.argv.length === 5) {
+   addPerson()
+}
+
+
+const url = `mongodb+srv://developer:${password}@mooc-p3-phonebook.nvtm7vy.mongodb.net/?retryWrites=true&w=majority&appName=mooc-p3-phonebook`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+
+
+
